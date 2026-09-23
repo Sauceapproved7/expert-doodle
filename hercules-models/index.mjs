b05304c470ce780df3fd826a76f55160017efaa7
Sauce <sauce718@outlook.com>
2026-09-23 15:37:10 -0400

    Add Hercules owned model plane v0.1

---
+++ hercules-models/index.mjs
@@ -0,0 +1,11 @@
+export {
+  MODEL_PLANE_VERSION,
+  MODEL_TASKS,
+  MODEL_STATES,
+  MODEL_ORIGINS,
+  normalizeModelManifest,
+  validateModelManifest,
+} from "./schema.mjs";
+export {HerculesModelRegistry} from "./registry.mjs";
+export {HerculesModelRouter} from "./router.mjs";
+export {HERCULES_MODEL_SLOTS} from "./catalog.mjs";
